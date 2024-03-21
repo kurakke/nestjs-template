85957439665bf472d91ae541dda205dd7618679e
kurakke <kcanggua@gmail.com>
2024-03-22 00:48:12 +0900

    setup: prisma-seed

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import {
+  initialize,
+  defineKurakkeFactory,
+  defineKurakkeChildFactory,
+} from '../fabbrica';
+
+const prisma = new PrismaClient();
+initialize({ prisma });
+
+const main = async () => {
+  const kurakkeFactory = defineKurakkeFactory({
+    defaultData: async ({ seq }) => {
+      return {
+        name: `Kurakke ${seq}`,
+        description: `Description of Kurakke ${seq}`,
+      };
+    },
+  });
+  const kurakkeChildFactory = defineKurakkeChildFactory({
+    defaultData: async ({ seq }) => {
+      return {
+        kurakke: kurakkeFactory,
+        name: `KurakkeChild ${seq}`,
+        description: `Description of KurakkeChild ${seq}`,
+      };
+    },
+  });
+  await kurakkeChildFactory.createList(10);
+};
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
