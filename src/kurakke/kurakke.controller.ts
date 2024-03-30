05ca0b4fb599e4e3ae9b752e096c1eb30f226233
kurakke <kcanggua@gmail.com>
2024-03-30 22:13:07 +0900

    add: create unit test for kurakke controller

--- src/kurakke/kurakke.controller.ts
+++ src/kurakke/kurakke.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { KurakkeService } from './kurakke.service';
 import { CreateKurakkeDto } from './dto/create-kurakke.dto';
+import { UpdateKurakkeDto } from './dto/update-kurakke.dto';
 
 @Controller('kurakke')
 export class KurakkeController {
@@ -16,8 +25,18 @@ export class KurakkeController {
     return this.kurakkeService.findAll();
   }
 
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateKurakkeDto: UpdateKurakkeDto) {
+    return this.kurakkeService.update(id, updateKurakkeDto);
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.kurakkeService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.kurakkeService.remove(id);
   }
 }
